57af944f6a5166111f5c8d210179635871d22a12
Angello <angello.zuloaga@utec.edu.pe>
2022-11-14 23:55:58 -0500

    asdfg

--- src/components/SubMenu.js
+++ src/components/SubMenu.js
@@ -81,3 +81,26 @@ const SubMenu = ({ item }) => {
 };
 
 export default SubMenu;
+
+/*
+function Example() {
+  const [showForm, setShowForm] = useState(false);
+
+  const showForm = () => {
+    setShowForm(!showForm);
+  }
+
+  return (
+    <div>
+      <form>
+        <button onClick={showForm}></button>
+      </form>
+
+      {showForm && (
+        <form>
+          ...
+        </form>
+      )}
+    </div>
+  )   
+}*/
