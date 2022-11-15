417b2fb836323065e5d8b451b5781fe166b0f375
neftali <neftali.calixto@utec.edu.pe>
2022-11-14 23:34:48 -0500

    estable-dasboard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router} from 'react-router-dom'
 import CalendarHeader from './components/CalendarHeader';
-import Weekview from './components/weekview';
 import Side from './components/side';
-
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
@@ -12,12 +11,10 @@ function App() {
         <Side />
         <div className="flex flex-columns">
           <CalendarHeader />
-          <div className='py-10 px-20 flex flex-1'>
-            <Weekview/>
+          <div className=' px-20 flex flex-1'>
+            <Dashboard/>
           </div>
-          
         </div>
-        
      </div>
     </Router>
   )
---
+++ src/components/appointments.js
@@ -0,0 +1,64 @@
+export const appointments = [
+    {
+      title: 'Book Flights to San Fran for Sales Trip',
+      startDate: new Date(2018, 6, 2, 12, 0),
+      endDate: new Date(2018, 6, 2, 13, 0),
+      id: 17,
+      location: 'Room 3',
+    }, {
+      title: 'Install New Router in Dev Room',
+      startDate: new Date(2018, 6, 2, 14, 30),
+      endDate: new Date(2018, 6, 2, 17, 30),
+      id: 18,
+      location: 'Room 2',
+    }, {
+      title: 'Approve Personal Computer Upgrade Plan',
+      startDate: new Date(2018, 6, 2, 16, 0),
+      endDate: new Date(2018, 6, 3, 9, 0),
+      id: 19,
+      location: 'Room 2',
+    }, {
+      title: 'Final Budget Review',
+      startDate: new Date(2018, 6, 3, 10, 15),
+      endDate: new Date(2018, 6, 3, 13, 35),
+      id: 20,
+      location: 'Room 1',
+    }, {
+      title: 'New Brochures',
+      startDate: new Date(2018, 6, 3, 14, 30),
+      endDate: new Date(2018, 6, 3, 15, 45),
+      id: 21,
+      location: 'Room 3',
+    }, {
+      title: 'Install New Database',
+      startDate: new Date(2018, 6, 3, 15, 45),
+      endDate: new Date(2018, 6, 4, 12, 15),
+      id: 22,
+      location: 'Room 3',
+    }, {
+      title: 'Approve New Online Marketing Strategy',
+      startDate: new Date(2018, 6, 4, 12, 35),
+      endDate: new Date(2018, 6, 4, 14, 15),
+      id: 23,
+      location: 'Room 3',
+    }, {
+      title: 'Upgrade Personal Computers',
+      startDate: new Date(2018, 6, 4, 15, 15),
+      endDate: new Date(2018, 6, 4, 20, 30),
+      id: 24,
+      location: 'Room 2',
+    }, {
+      title: 'Customer Workshop',
+      startDate: new Date(2018, 6, 5, 6, 0),
+      endDate: new Date(2018, 6, 5, 14, 20),
+      id: 25,
+      location: 'Room 1',
+    }, {
+      title: 'Customer Workshop',
+      startDate: new Date(2018, 6, 5, 14, 35),
+      endDate: new Date(2018, 6, 5, 16, 20),
+      id: 26,
+      location: 'Room 1',
+    },
+  ];
+  
