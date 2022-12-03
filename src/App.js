6f8423eb2cecdd8d7025214ee4e6d8c7d20e555e
neftali <neftali.calixto@utec.edu.pe>
2022-12-03 16:16:33 -0500

    details

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router} from 'react-router-dom'
 import CalendarHeader from './components/CalendarHeader';
-import Side from './components/side';
 import Dashboard from "./Dashboard";
 import Sidebar from "./components/Activities"
 import Results from "./components/Results"
@@ -15,12 +14,16 @@ function App() {
         <Sidebar />
         <div className="flex flex-columns">
           <CalendarHeader />
-          <div className=' px-20 flex flex-1'>
+          <div className=' px-2 flex flex-1'>
             <Dashboard/>
           </div>
+            <Results />
         </div>
-        <Results />
-     </div>
+      </div>
+      
+      
+      
+        
     </Router>
   )
 }
